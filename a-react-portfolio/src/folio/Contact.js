f4dc95f924638513d58c947ad7f85dc99efb96ba
Kristabell Johnson <bell77iam@gmail.com>
2021-03-07 13:50:33 -0700

    Added several react databases with same name saving and going to try again

--- a-react-portfolio/src/folio/Contact.js
+++ a-react-portfolio/src/folio/Contact.js
@@ -41,7 +41,7 @@ class Contact extends React.Component {
             disabled: true
         });
 
-        Axios.post('http://localhost:3030/api/email', this.state)
+        Axios.post('http://localhost:3000/api/email', this.state)
             .then(res => {
                 if (res.data.success) {
                     this.setState({
